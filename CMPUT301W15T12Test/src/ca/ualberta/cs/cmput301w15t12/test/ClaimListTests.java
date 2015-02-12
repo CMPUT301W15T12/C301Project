713f05ce86123bcf90c49beddf99f106c16f8ac9
Raymond Lieu <rlieu@ualberta.ca>
2015-02-12 00:39:51 -0700

    Added delete claim and expense test cases

--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ClaimListTests.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ClaimListTests.java
@@ -8,5 +8,15 @@ public class ClaimListTests extends TestCase
 	//TODO constructor, setup?
 	//TODO tests for get/add/remove claims
 	//TODO tests for get/count list
-
+	
+	public void deleteClaim() {
+		ClaimItem claim = new ClaimItem();
+		ClaimsList claimsList = new ClaimsList();
+		
+		claimsList.add(claim);
+		assertTrue("Claim was not added to claims list", claimsList.size() > 0 && claimsList.contains(claim));
+		
+		claimsList.remove(claim);
+		assertTrue("Claim was not removed from claims list", claimsList.size() == 0 && !claimsList.contains(claim));
+	}
 }
--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ExpenseListTests.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ExpenseListTests.java
@@ -8,4 +8,15 @@ public class ExpenseListTests extends TestCase
 	//TODO constructor, setup?
 	//TODO tests for get/add/remove items
 	//TODO tests for get/count list
+	
+	public void deleteExpense() {
+		ExpenseItem expense = new ExpenseItem();
+		ExpenseList expenseList = new ExpenseList();
+		
+		expenseList.add(expense);
+		assertTrue("Expense was not added to expense list", expenseList.size() > 0 && expenseList.contains(expense));
+		
+		expenseList.remove(expense);
+		assertTrue("Expense was not removed from expense list", expenseList.size() == 0 && !expenseList.contains(expense));
+	}
 }
