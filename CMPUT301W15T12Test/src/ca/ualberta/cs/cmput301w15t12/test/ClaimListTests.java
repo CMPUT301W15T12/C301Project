95cd406623c03478f287d7360503f0a5550444c4
megsum <msumner@ualberta.ca>
2015-02-13 00:56:20 -0700

    added more tests for claims

--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ClaimListTests.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ClaimListTests.java
@@ -9,14 +9,31 @@ public class ClaimListTests extends TestCase
 	//TODO tests for get/add/remove claims
 	//TODO tests for get/count list
 	
+	//constructor
+	public ClaimListTests(){
+		super(ClaimList.class);
+	}
+	
 	public void deleteClaim() {
 		ClaimItem claim = new ClaimItem();
-		ClaimsList claimsList = ClaimsListController.getClaims();
+		ClaimList claimList = ClaimListController.getClaims();
 		
-		claimsList.add(claim);
-		assertTrue("Claim was not added to claims list", claimsList.size() > 0 && claimsList.contains(claim));
+		claimList.add(claim);
+		assertTrue("Claim was not added to claims list", claimList.size() > 0 && claimList.contains(claim));
 		
-		claimsList.remove(claim);
-		assertTrue("Claim was not removed from claims list", claimsList.size() == 0 && !claimsList.contains(claim));
+		claimList.remove(claim);
+		assertTrue("Claim was not removed from claims list", claimList.size() == 0 && !claimList.contains(claim));
+	}
+	
+	//US02.01.01 - list all my expense claims, showing for each claim: the starting date of travel, the destination(s) of travel, the claim status, tags, and total currency amounts
+	public void testListClaims(){
+		ClaimItem claim = new ClaimItem();
+		ClaimList claimList = ClaimListController.getClaims();
+		
+	}
+	
+	//US02.02.01 - list of expense claims to be sorted by starting date of travel, in order from most recent to oldest
+	public void testSorted(){
+		
 	}
 }
--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ClaimTests.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ClaimTests.java
@@ -6,20 +6,20 @@ import junit.framework.TestCase;
 public class ClaimTests extends TestCase
 {
 	public ClaimTests(){
-		super(Claim.class);
+		super(ClaimItem.class);
 	}
 	//Tests for equal/submit/edit/getSum
 	//Test for equals, claim name must be unique
 	public void testEquals(){
-		Claim claim1 = new Claim();
-		Claim claim2 = new Claim();
+		ClaimItem claim1 = new ClaimItem();
+		ClaimItem claim2 = new ClaimItem();
 		assertFalse("names are the same", claim1.getName().equals(claim2.getName()));
 	}
 	
 	//US01.01.01 - expense claim that records my name, a starting date of travel, and an ending date of travel
 	//US01.02.01 - record one or more destinations of travel and an associated reason for travel to each destination
 	public void testClaim(){
-		Claim claim = new Claim();
+		ClaimItem claim = new ClaimItem();
 		assertNotNull("Name not initialized", claim.getName());
 		assertNotNull("Start Date not initialized", claim.getStartDate());
 		assertNotNull("End Date not initialized", claim.getEndDate());
@@ -39,6 +39,11 @@ public class ClaimTests extends TestCase
 	
 	//US01.06.01 - entered information to be remembered, so that I do not lose data
 	public void testInfoSaved(){
+		
+	}
+	
+	//US09.01.01 - make and work on expense claims and items while offline, and push application and expense information online once I get connectivity.
+	public void testOffline(){
 		
 	}
 }
