38636e17fc9cd59be54263cc55c5906727bbb682
qsjiang <jqiu321@gmail.com>
2015-02-13 06:12:36 -0700

    create ReceiptPhotoCRUDTest to test the receipt photo create/view/edit/delete feature inside item_page

---
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ReceiptPhotoCRUDTest.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.cmput301w15t12.test;
+
+import ca.ualberta.cs.cmput301w15t12.ExpenseItemActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ReceiptPhotoCRUDTest extends ActivityInstrumentationTestCase2<ExpenseItemActivity> {
+
+	public ReceiptPhotoCRUDTest() {
+		super(ExpenseItemActivity.class);
+		// TODO Auto-generated constructor stub
+	}
+
+}
