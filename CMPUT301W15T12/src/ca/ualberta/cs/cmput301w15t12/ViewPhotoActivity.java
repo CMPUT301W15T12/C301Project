0bd343171491870ad97ca1ed1ee7d92f7347a3e6
qsjiang <qsjiang@ualberta.ca>
2015-02-13 04:15:49 -0700

    set up blank activity for ViewPhotoActivity, set up blank testing class for ViewPhotoActivity

---
+++ CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/ViewPhotoActivity.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.cmput301w15t12;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewPhotoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_photo);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_photo, menu);
+		return true;
+	}
+
+}
---
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ViewPhotoActivityTest.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cs.cmput301w15t12.test;
+
+import ca.ualberta.cs.cmput301w15t12.ViewPhotoActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ViewPhotoActivityTest extends ActivityInstrumentationTestCase2<ViewPhotoActivity> {
+
+	public ViewPhotoActivityTest() {
+		super(ViewPhotoActivity.class);
+	}
+
+}
