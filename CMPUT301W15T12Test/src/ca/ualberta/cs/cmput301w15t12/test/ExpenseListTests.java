4a96b9bd78d990fd59dba5fb40b3512386a567c1
LeahGabrielle <olexson@ualberta.ca>
2015-02-13 15:21:16 -0700

    ExpenseListTests error free now

--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ExpenseListTests.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ExpenseListTests.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.cmput301w15t12.test;
 
+import ca.ualberta.cs.cmput301w15t12.AlreadyExistsException;
+import ca.ualberta.cs.cmput301w15t12.Claim;
 import ca.ualberta.cs.cmput301w15t12.ExpenseItem;
 import ca.ualberta.cs.cmput301w15t12.ExpenseList;
 import java.math.BigDecimal;
@@ -14,17 +16,19 @@ public class ExpenseListTests extends TestCase
 	//TODO tests for get/count list
 	
 	//US04.07.01 - able to delete an expense item when allowed
-	public void deleteExpense() {
+	public void deleteExpense() throws AlreadyExistsException {
 		Date date = new Date();
 		BigDecimal amount = new BigDecimal(45.50);
+		Date date1 = new Date();
+		Date date2 = new Date();
 		ExpenseItem expense = new ExpenseItem("name","air fare","description","USD",amount,date,false);
-		ClaimItem claim = new ClaimItem();
-		ExpenseList expenseList = claim.getExpenses();
+		Claim claim = new Claim("Claim",date1,date2,"description","Submitted");
+		ExpenseList expenseList = claim.getExpenses(claim.getExpenseItems());
 		
 		expenseList.add(expense);
 		assertTrue("Expense was not added to expense list", expenseList.size() > 0 && expenseList.contains(expense));
 		
-		expenseList.remove(expense);
+		expenseList.removeItem(expense.getName());
 		assertTrue("Expense was not removed from expense list", expenseList.size() == 0 && !expenseList.contains(expense));
 	}
 }
