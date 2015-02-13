6ac5d4c7bf5abe49274e9d3f1f069cbf828f10ec
qsjiang <jqiu321@gmail.com>
2015-02-13 06:48:51 -0700

    add test cases into ReceiptPhotoCRUDTest.java along with necessary code to make it compile

--- CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/ExpenseItemActivity.java
+++ CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/ExpenseItemActivity.java
@@ -2,6 +2,7 @@ package ca.ualberta.cs.cmput301w15t12;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.view.Menu;
 
 public class ExpenseItemActivity extends Activity {
@@ -17,6 +18,16 @@ public class ExpenseItemActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.expense_item, menu);
 		return true;
+	}
+
+	public void deleteReceiptPhoto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Bitmap getReceiptPhoto() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ReceiptPhotoCRUDTest.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ReceiptPhotoCRUDTest.java
@@ -1,7 +1,16 @@
+/* Description: jUnit tests for testing CRUD related features inside item_page
+ * Author: Qiushi Jiang
+ * Email: qsjiang@ualberta.ca
+ */
 package ca.ualberta.cs.cmput301w15t12.test;
 
 import ca.ualberta.cs.cmput301w15t12.ExpenseItemActivity;
+import ca.ualberta.cs.cmput301w15t12.R;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.ImageButton;
+
 
 public class ReceiptPhotoCRUDTest extends ActivityInstrumentationTestCase2<ExpenseItemActivity> {
 
@@ -9,5 +18,26 @@ public class ReceiptPhotoCRUDTest extends ActivityInstrumentationTestCase2<Expen
 		super(ExpenseItemActivity.class);
 		// TODO Auto-generated constructor stub
 	}
-
+	//[US06.01.01] - Taking photograph of a receipt
+	public void testImageButton(){
+		ExpenseItemActivity activity = startExpenseItemActivity();
+		View buttonImage=activity.findViewById(R.id.buttonImage);
+		assertTrue("receiptImageView should be an instanceof ImageButton",buttonImage.getClass() == ImageButton.class);
+	}	
+	
+	//[US06.03.01] - Retaking photograph of a receipt
+	public void testDeleteImage(){
+		ExpenseItemActivity activity = startExpenseItemActivity();
+		activity.deleteReceiptPhoto();
+		assertTrue("After deleting the receiptphoto, getReceiptPhoto() should return null",activity.getReceiptPhoto()==null);
+	}
+	
+	
+	//see [US06.02.01] and [US06.04.01] in ViewPhotoActivityTest.java
+	private ExpenseItemActivity startExpenseItemActivity(){
+		Intent intent = new Intent();
+		setActivityIntent(intent);
+		return (ExpenseItemActivity) getActivity();
+	}
+	
 }
