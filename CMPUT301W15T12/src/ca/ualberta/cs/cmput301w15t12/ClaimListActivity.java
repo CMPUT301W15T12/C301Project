9a981b8759d797ff09f803a7167fccfb4253c35f
vanbelle <vanbelle.ualberta.ca>
2015-03-04 15:46:34 -0700

    worked on activities

--- CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/ClaimListActivity.java
+++ CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/ClaimListActivity.java
@@ -12,7 +12,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class ClaimListActivity extends Activity {
 
