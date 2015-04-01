b8f0d88d48aa6613b8391447925d5e5d5a9399c6
megsum <msumner@ualberta.ca>
2015-04-01 13:48:03 -0600

    can now add marker to map

--- CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/MapActivity.java
+++ CMPUT301W15T12/src/ca/ualberta/cs/cmput301w15t12/MapActivity.java
@@ -1,16 +1,23 @@
 package ca.ualberta.cs.cmput301w15t12;
 
-import android.location.Location;
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.widget.Toast;
+import java.util.ArrayList;
 
+import org.osmdroid.DefaultResourceProxyImpl;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.Projection;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
-import org.osmdroid.views.overlay.SimpleLocationOverlay;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 //https://github.com/IanDarwin/Android-Cookbook-Examples/blob/master/OSMIntro/src/com/OSM/OSM.java 2015/03/26
 public class MapActivity extends Activity
@@ -18,8 +25,9 @@ public class MapActivity extends Activity
 	private MapView mapView;
 	private MapController mapController;
 	private ScaleBarOverlay mScaleBarOverlay;
-	private SimpleLocationOverlay mMyLocationOverlay;
+	private ItemizedIconOverlay<OverlayItem> mMyLocationOverlay;
 	private Location location;
+	private DefaultResourceProxyImpl resourceProxy;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -32,13 +40,14 @@ public class MapActivity extends Activity
 		 mapView.setBuiltInZoomControls(true);
 		 mapView.setMultiTouchControls(true);
 		 mapController = (MapController) this.mapView.getController();
-		 mapController.setZoom(2);
+		 mapController.setZoom(8);
 		 
-		 
+	     GeoPoint mapCenter = new GeoPoint(53554070, -2959520); 
+	     mapController.setCenter(mapCenter);
 		 String option = getIntent().getExtras().getString("option");
+	     mapView.invalidate();
 		 if(option.equals("add")){
-			 //TODO get Location
-			 //returnLocation();
+			 //todo  
 		 } else {
 			 ClaimListController CLC = new ClaimListController();
 			 int id = getIntent().getIntExtra("claim_id",0);
@@ -46,6 +55,29 @@ public class MapActivity extends Activity
 			 //TODO show Locations
 		 }
 		 
+	}
+	
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+	    int actionType = ev.getAction();
+	    switch (actionType) {
+	    case MotionEvent.ACTION_UP:
+	            Projection proj = mapView.getProjection();
+	            GeoPoint loc = (GeoPoint) proj.fromPixels((int)ev.getX(), (int)ev.getY()); 
+	            GeoPoint overlayPoint = new GeoPoint(loc.getLatitudeE6()+365000,loc.getLongitudeE6()-1000);
+	            String longitude = Double.toString(((double)loc.getLongitudeE6())/1000000);
+	            String latitude = Double.toString(((double)loc.getLatitudeE6())/1000000);
+	            ArrayList<OverlayItem> overlays = new ArrayList<OverlayItem>();
+	            overlays.add(new OverlayItem("New Overlay", "Overlay Description", overlayPoint));
+	            resourceProxy = new DefaultResourceProxyImpl(getApplicationContext());
+	            this.mMyLocationOverlay = new ItemizedIconOverlay<OverlayItem>(overlays, null, resourceProxy);
+	            this.mapView.getOverlays().add(this.mMyLocationOverlay);
+	            mapView.invalidate();
+	            Toast toast = Toast.makeText(getApplicationContext(), "Longitude: "+ longitude +" Latitude: "+ latitude , Toast.LENGTH_SHORT);
+	            toast.show();
+
+	    }
+	return super.dispatchTouchEvent(ev);
 	}
 
 	public void setPoint(){
