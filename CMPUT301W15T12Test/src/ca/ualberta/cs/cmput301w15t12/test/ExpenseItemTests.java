04f1d210c8733d4e356c70ca34e3215d520c9ddc
IchigoKIl <rlieu@ualberta.ca>
2015-04-02 17:00:27 -0600

    Updated ExpenseItemTest

--- CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ExpenseItemTests.java
+++ CMPUT301W15T12Test/src/ca/ualberta/cs/cmput301w15t12/test/ExpenseItemTests.java
@@ -9,150 +9,127 @@ import ca.ualberta.cs.cmput301w15t12.ExpenseItem;
 import ca.ualberta.cs.cmput301w15t12.ExpenseItemActivity;
 
 public class ExpenseItemTests extends ActivityInstrumentationTestCase2<ExpenseItemActivity> {
-	
-//	ExpenseItemActivity activity;
-//	Instrumentation instrumentation;
-	
 	public ExpenseItemTests(){
 		super(ExpenseItemActivity.class);
 	}
 	
 	protected void setUp() throws Exception {
 		super.setUp();
-//		instrumentation = getInstrumentation();
-//		activity = getActivity();
 	}
 	
 	//US04.01.01 - expense item has date, category, description, amount, currency
 	public void testExpenseItem(){
-		Date date = new Date();
-		BigDecimal amount = new BigDecimal(45.50);
-		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD",amount,date, false);
-		assertNotNull("date not initialized",expenseItem.getDate());
-		assertNotNull("category not initialized",expenseItem.getCategory());
-		assertNotNull("description not initialized",expenseItem.getDescription());
-		assertNotNull("amount not initialized",expenseItem.getAmount());
-		assertNotNull("currency not initialized",expenseItem.getCurrency());
-		assertNotNull("name not initialized",expenseItem.getName());
+		// Make new expense item
+		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD", new BigDecimal(66.69), new Date(), false);
+		
+		// Make sure everything is initialized properly
+		assertNotNull("date not initialized", expenseItem.getDate());
+		assertNotNull("category not initialized", expenseItem.getCategory());
+		assertNotNull("description not initialized", expenseItem.getDescription());
+		assertNotNull("amount not initialized", expenseItem.getAmount());
+		assertNotNull("currency not initialized", expenseItem.getCurrency());
+		assertNotNull("name not initialized", expenseItem.getName());
 	}
 	
 	//US04.02.01 - category is one of air fare, round transport, vehicle rental, 
-	//private automobile, fuel, parking, registration, accommodation, meal, or supplies.
-	//Valid categories are stored in the expenseItem class in a static final array list
+	// private automobile, fuel, parking, registration, accommodation, meal, or supplies.
+	// Valid categories are stored in the expenseItem class in a static final array list
 	public void testCategory(){
-		Date date = new Date();
-		BigDecimal amount = new BigDecimal(45.50);
-		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD",amount,date, false);
+		// Make new expense item
+		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD", new BigDecimal(66.69), new Date(), false);
 		
 		try {
-			// Try no category (should still be valid)
+			// Try setting no category (should still be valid)
 			expenseItem.setCategory(null);
 			expenseItem.setCategory("");
 		} catch (Exception e) {
-			// Should not throw exception with no category
-			fail("Shouldn't have thrown an exception");
+			// Exception thrown when setting no category
+			fail("Shouldn't have thrown an exception with no category");
 		}
 		
 		try {
-			// Try all valid categories 
+			// Try setting all valid categories 
 			for (String category : ExpenseItem.getCategories()) {
 				expenseItem.setCategory(category);
 			}
 		} catch (Exception e){
-			// Should not throw exception with valid categories
-			fail("Shouldn't have thrown an exception");
+			// Exception thrown when setting valid category
+			fail("Shouldn't have thrown an exception with valid categories");
 		}
 		
 		try {
-			// Try invalid category
+			// Try setting invalid category
 			expenseItem.setCategory("Invalid");
 			
-			// Should throw an exception
-			fail("Should have thrown an exception");
+			// Exception not thrown when setting invalid category
+			fail("Should have thrown an exception with an invalid category");
 		} catch (Exception e) {
-			// Working 
+			// Exception thrown when setting invalid category
 		}
 	}
 	
 	//US04.03.01 - currency is one of CAD, USD, EUR, GBP, CHF, JPY, CNY
 	//Valid currencies are stored in the expenseItem class in a static final array list
 	public void testCurrency(){
-		Date date = new Date();
-		BigDecimal amount = new BigDecimal(45.50);
-		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","CAD",amount,date, false);
+		// Make new expense item
+		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD", new BigDecimal(66.69), new Date(), false);
 		
 		try {
-			// Try no currency (should still be valid)
+			// Try setting no currency (should still be valid)
 			expenseItem.setCurrency(null);
 			expenseItem.setCurrency("");
 		} catch (Exception e) {
-			// Should not throw exception with no currency
-			fail("Shouldn't have thrown an exception");
+			// Exception thrown when setting no currency
+			fail("Shouldn't have thrown an exception with no currency");
 		}
 		
 		try {
-			// Try all valid currencies
+			// Try setting all valid currencies
 			for (String currency : ExpenseItem.getCurrencies()) {
 				expenseItem.setCurrency(currency);
 			}
 		} catch (Exception e){
-			// Should not throw exception with valid currencies
-			fail("Shouldn't have thrown an exception");
+			// Exception thrown when setting valid currency
+			fail("Shouldn't have thrown an exception with valid currencies");
 		}
 		
 		try {
 			// Try an invalid currency
 			expenseItem.setCurrency("Invalid");
 			
-			// Should throw an exception with invalid categories
-			fail("Should have thrown an exception");
+			// Exception not thrown when setting invalid currency
+			fail("Should have thrown an exception with an invalid currency");
 		} catch (Exception e) {
-			// Working 
+			// Exception thrown when setting invalid category
 		}
 	}
 	
 	//US04.04.01 - expense item can be flagged
 	public void testFlag(){
-		Date date = new Date();
-		BigDecimal amount = new BigDecimal(45.50);
-		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD",amount,date, false);
+		// Make new expense item
+		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD", new BigDecimal(66.69), new Date(), false);
 		
+		// Toggle flag
 		expenseItem.setFlag(true);
-		//passes if item has a flag
-		assertEquals("Item does not have a flag", true, expenseItem.getFlag());
-		
+		assertTrue("Item does not have a flag when it should", expenseItem.getFlag());
 		expenseItem.setFlag(false);
-		//passes if item does not have a flag
-		assertEquals("Item does have a flag", false, expenseItem.getFlag());
+		assertFalse("Item does have a flag when it shouldn't", expenseItem.getFlag());
 	}
 	
 	//US04.05.01 - able to view an expense item and its details
 	public void testViewItem(){
-		//ExpenseItemActivity activity = startExpenseItemActivity();
-		//final TextView decorView = (TextView) activity.getWindow().getDecorView();
-		
-//		TextView nameView = (TextView) activity.findViewById(R.id.textItemName);
-//		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),nameView);
-		
-//		TextView categoryView = (TextView) activity.findViewById(R.id.textCategory);
-//		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),categoryView);
-//		TextView descriptionView = (TextView) activity.findViewById(R.id.textItemDescription);
-//		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),descriptionView);
-//		TextView currencyView = (TextView) activity.findViewById(R.id.textItemCurrency);
-//		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),currencyView);
-//		TextView dateView = (TextView) activity.findViewById(R.id.textDate);
-//		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),dateView);
-//		CheckBox flagView = (CheckBox) activity.findViewById(R.id.checkBox1);
-//		ViewAsserts.assertOnScreen(activity.getWindow().getDecorView(),flagView);	
 	}
 	
 	//US04.06.01 - able to edit an expense item when allowed - setters
 	public void testEditItem(){
+		// Make new expense item
 		Date date = new Date();
-		BigDecimal amount = new BigDecimal(45.50);
-		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD",amount,date, false);
 		Date date2 = new Date();
-		BigDecimal amount2 = new BigDecimal(800.23);
+		BigDecimal amount = new BigDecimal(66.69);
+		BigDecimal amount2 = new BigDecimal(666.9);
+		ExpenseItem expenseItem = new ExpenseItem("name","Air Fare","description","USD", amount, date, false);
+
+		// Edit expense item
 		expenseItem.setName("name2");
 		expenseItem.setCategory("Parking");
 		expenseItem.setDescription("description2");
@@ -161,20 +138,15 @@ public class ExpenseItemTests extends ActivityInstrumentationTestCase2<ExpenseIt
 		expenseItem.setDate(date2);
 		expenseItem.setFlag(true);
 		
-		assertEquals("date not initialized",date2,expenseItem.getDate());
-		assertEquals("category not initialized","Parking",expenseItem.getCategory());
-		assertEquals("description not initialized","description2",expenseItem.getDescription());
-		assertEquals("amount not initialized",amount2,expenseItem.getAmount());
-		assertEquals("currency not initialized","CAD",expenseItem.getCurrency());
-		assertEquals("name not initialized","name2",expenseItem.getName());
+		// Make sure expense item is updated correctly
+		assertTrue("name not updated", expenseItem.getName() == "name2");
+		assertTrue("date not updated", expenseItem.getDate() != date && expenseItem.getDate() == date2);
+		assertTrue("category not updated", expenseItem.getCategory() == "Parking");
+		assertTrue("description not updated", expenseItem.getDescription() == "description2");
+		assertTrue("amount not updated", expenseItem.getAmount() != amount && expenseItem.getAmount() == amount2);
+		assertTrue("currency not updated", expenseItem.getCurrency() == "CAD");
+		assertTrue("flag not updated", expenseItem.getFlag());
 	}
-
-	private ExpenseItemActivity startExpenseItemActivity(){
-		Intent intent = new Intent();
-		setActivityIntent(intent);
-		return (ExpenseItemActivity) getActivity();
-	}
-	
 	
 	//US04.08.01 - minimal required navigation in user interface when entering an item - test manually
 }
